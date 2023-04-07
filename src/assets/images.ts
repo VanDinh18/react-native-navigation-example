9eaa0c1624d28b575011f3f26174be7472b7e954
dinh.nguyen.18 <dinh.nguyen.18@Dinh-MacBook-Pro.local>
2023-04-07 23:13:20 +0700

    feat: update bottom tab

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -1,9 +1,14 @@
+import IcTabHome from './svg/ic-tab-home.svg';
+
 const Icons = {
   avatar: require('./icons/ic_avatar.png'),
+  IcTabHome: require('./icons/ic-tab-home.png'),
 };
 
 const Photos = {};
 
-const Svg = {};
+const Svg = {
+  IcTabHome,
+};
 
 export {Icons, Photos, Svg};
